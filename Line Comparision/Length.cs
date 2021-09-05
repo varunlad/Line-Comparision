0079cc0036dd5850db82b1d505199fb10b2ebf0f
varun <varunlad5@gmail.com>
2021-09-05 17:54:47 +0530

    [varun] User Case 1 Length of a Line

---
+++ Line Comparision/Length.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparision
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first point :");
+            float x1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second point :");
+            float y1 = float.Parse(Console.ReadLine()); ;
+            Console.WriteLine("Enter Third point :");
+            float x2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Fourth point :");
+           float y2 = float.Parse(Console.ReadLine());
+           float v = x2 - x1;
+           float w = y2 - y1;
+            Console.WriteLine("The length of a line is :");
+            Console.WriteLine(System.Math.Sqrt(v * v + w * w)) ;
+            Console.ReadLine();
+        }
+    }
+}
